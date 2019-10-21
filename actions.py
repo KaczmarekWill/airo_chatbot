addcc3c1e5408675aec9a95b74919d2f087fa923
Will Kaczmarek <kaczmarekwill@gmail.com>
2019-10-21 12:03:20 -0400

    Updated actions template

--- actions.py
+++ actions.py
@@ -1,27 +1,13 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
 
+# Sample action showing interactions between the script and the bot
 
-# This is a simple example for a custom action which utters "Hello World!"
+#class ActionName(Action):
+#	def name(self):
+#		return 'action_name' <-- This must match an action in the domain
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message("Hello World!")
-#
-#         return []
+#	def run(self, dispatcher, tracker, domain):
+#		variable = tracker.get_slot('SLOT_NAME') <-- Get a value from a slot
+#		dispatcher.utter_message('STRING TO PRINT') <-- Have bot utter message
+#		return [SlotSet('SLOT_NAME', 'WHAT TO SET SLOT TO')] <-- Set a value to a slot
